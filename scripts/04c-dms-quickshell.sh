15f98522f0c272becef7ebdd4ea885540548a835
shorinkiwata <fcl709@outlook.com>
2026-02-17 22:49:57 +0900

    [FIX] flatpak主题

--- scripts/04c-dms-quickshell.sh
+++ scripts/04c-dms-quickshell.sh
@@ -456,10 +456,12 @@ log "Configuring Flatpak for Shorin DMS..."
 if command -v flatpak &>/dev/null; then
 exe as_user yay -S --noconfirm --needed bazaar
 as_user flatpak override --user --filesystem=xdg-data/themes
+as_user flatpak override --user --filesystem="$HOME_DIR/.themes"
 as_user flatpak override --user --filesystem=xdg-config/gtk-4.0
 as_user flatpak override --user --filesystem=xdg-config/gtk-3.0
 as_user flatpak override --user --env=GTK_THEME=adw-gtk3-dark
 as_user flatpak override --user --filesystem=xdg-config/fontconfig
+ln -sf /usr/share/themes "$HOME_DIR/.local/share/themes"
 fi
 
 # === matugen 配置  ===
